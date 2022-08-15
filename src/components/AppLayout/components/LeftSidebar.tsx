61840cfbad3f6d251e91143492d487d6939aeea1
Nikhil <nikhil@tartanhq.com>
2022-08-15 19:11:08 +0530

    app layout

--- src/components/AppLayout/components/LeftSidebar.tsx
+++ src/components/AppLayout/components/LeftSidebar.tsx
@@ -4,7 +4,11 @@ export default function LeftSidebar() {
   return (
     <div>
       <div className="w-[300px] h-[calc(100vh)] bg-[#3E113F] sticky top-0 text-[#fff] pt-[40px] overflow-y-scroll">
-      
+        <div className="border-y-[1px] border-[#743f75] py-[8px] px-[16px]">
+          <p className="text-[#fff] text-[1.125rem] leading-[1.369rem] font-bold tracking-[0.04em]">
+            Organization
+          </p>
+        </div>
       </div>
     </div>
   );
